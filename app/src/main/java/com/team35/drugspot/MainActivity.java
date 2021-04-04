f13e89368d434692bfdb8dda8932053cefefb721
YAGAMI <msujikanth@gmail.com>
2021-04-04 18:39:19 +0530

    index

--- app/src/main/java/com/team35/drugspot/MainActivity.java
+++ app/src/main/java/com/team35/drugspot/MainActivity.java
@@ -2,7 +2,6 @@ package com.team35.drugspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
