64d65b53d8be62c95f8a1b62f3c29e78b020a2b9
YAGAMI <msujikanth@gmail.com>
2021-04-08 12:36:57 +0530

    UI Changes

---
+++ app/src/main/java/com/team35/drugspot/RegisterRepActivity.java
@@ -0,0 +1,14 @@
+package com.team35.drugspot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterRepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_rep);
+    }
+}
